5a4aacf6fd94fa0df2f9f4bfa3ec7b76eccc0f5c
haebeompark <beom0528@naver.com>
2015-12-21 19:24:09 +0900

    block and tetromino class
    no method. i`ll add methods soon.

---
+++ Block.java
@@ -0,0 +1,14 @@
+package tetris;
+import java.awt.Point;
+import java.awt.Color;
+public class Block {
+	private Point point;
+	public Point getPoint(){
+		return point;
+	}
+	public void setPoint(Point point){
+		this.point = point;
+	}
+	
+	
+}
---
+++ Tetromino.java
@@ -0,0 +1,6 @@
+package tetris;
+import java.awt.Point;
+public class Tetromino {
+/******Tetromino is consist by 4blocks so it use block instances(about Array)*******/
+/******--> A tetrimino has 4 points, and 4 blocks and 4 color(it mean 4 block instances)*******/
+}
